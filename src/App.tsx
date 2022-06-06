12876bd0dd6889e6e692ff8d5d85f44c144af969
otrachea <xtrachea@gmail.com>
2022-06-06 16:30:55 -0400

    Set background color and cleanup remaining default boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="dark:bg-slate-800 min-h-screen">
       <h1 className="text-3xl font-bold">Hello world!</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+
     </div>
   );
 }
