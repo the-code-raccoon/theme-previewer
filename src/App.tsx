7430d9f8dda67e9b31b8c52522adf7b6cbc5a9ed
otrachea <xtrachea@gmail.com>
2022-06-06 17:07:10 -0400

    Add sidebar and one type of card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Card from "./components/Card";
+import SideBar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="dark:bg-slate-800 min-h-screen">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-
+    <div className="bg-slate-800 min-h-screen flex">
+      <SideBar />
+      <div className="p-10">
+        <Card />
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+      </div>
     </div>
   );
 }
