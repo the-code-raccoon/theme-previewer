4d104cf6bde9a5163a069b5c7ca1f70e372c6fc1
otrachea <xtrachea@gmail.com>
2022-06-07 14:41:16 -0400

    Add custom profile cards

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,37 @@ import ProfileCard from "./components/ProfileCard";
 import SideBar from "./components/Sidebar";
 
 export default function App() {
+  const profiles = [
+    {
+      name: "Alex Jones",
+      colour: "sky-300",
+      pic: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      name: "Jen Yang",
+      colour: "orange-300",
+      pic: "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  ];
+
   return (
     <div className="bg-slate-800 min-h-screen flex w-100">
       <SideBar />
       <div className="basis-full">
         <Banner />
-        <div className="p-10 columns-1 xl:columns-3">
+        <div className="container p-10 columns-1 xl:columns-3 mx-auto">
           <CardImageSide />
           <CardImageTop />
-          <ProfileCard />
+          {profiles.map((profile) => {
+            return (
+              <ProfileCard
+                key={profile.name}
+                name={profile.name}
+                colour={profile.colour}
+                pic={profile.pic}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,25 +1,32 @@
-export default function ProfileCard() {
+interface ProfileCardProps {
+  name: string;
+  colour: string;
+  pic: string;
+}
+
+export default function ProfileCard(props: ProfileCardProps) {
+  const { name, colour, pic } = props;
+
   return (
-    <div className="max-w-md mx-auto mb-10 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div className="mx-auto mb-10 bg-white rounded-xl shadow-md overflow-hidden w-72">
       <div>
         <div>
-          <div className="h-56 w-full bg-sky-300 flex items-center justify-center">
+          <div
+            className={`h-56 w-full bg-${colour} flex items-center justify-center`}
+          >
             <img
               className="inline-block h-24 w-24 rounded-full ring-2 ring-white"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              src={pic}
               alt=""
             />
           </div>
         </div>
         <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            Case study
-          </div>
           <a
             href="/"
             className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
           >
-            Finding customers for your new business
+            {name}
           </a>
           <p className="mt-2 text-slate-500">
             Getting a new business off the ground is a lot of hard work. Here
